ad7b69c736b50217aaaa5edada304798d3f80dc4
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2019-09-17 12:45:10 +0300

    fix

--- src/tasks/add-file.ts
+++ src/tasks/add-file.ts
@@ -16,28 +16,55 @@ export class AddFile implements AbstractRunner<{ ETag: string }> {
       file,
       nextOrDefault('--zip-name', './rxdi.tar.gz')
     );
-    const config = Container.get(Config);
-    return new Promise<{ ETag: string }>((resolve, reject) => {
-      request.post(
-        {
-          headers: {
-            authorization:
-              nextOrDefault('--authorization', null) || config.token
+    function postRequest(url: string) {
+      return new Promise<{ ETag: string }>((resolve, reject) => {
+        request.post(
+          {
+            headers: {
+              authorization:
+                nextOrDefault('--authorization', null) || config.token
+            },
+            url,
+            formData: {
+              file: createReadStream(archivePath)
+            }
           },
-          url:
-            nextOrDefault('--url', null) ||
-            config.registry || 'http://0.0.0.0:9000/upload',
-          formData: {
-            file: createReadStream(archivePath)
+          (error, response, body) => {
+            if (error) {
+              return reject(error);
+            }
+            resolve(body);
           }
-        },
-        (error, response, body) => {
-          if (error) {
-            return reject(error);
-          }
-          resolve(body);
+        );
+      });
+    }
+    const config = Container.get(Config);
+    let data: any;
+
+    try {
+      const endpoint =
+        nextOrDefault('--url', null) ||
+        config.registry ||
+        'http://0.0.0.0:9000/upload';
+      console.log(`Uploading file to endpoint: ${endpoint}`);
+      data = await postRequest(endpoint);
+      if (typeof(data) === 'string') {
+        const parse =  JSON.parse(data);
+        if (parse.statusCode && parse.statusCode !== '200') {
+          throw new Error(data);
         }
-      );
-    });
+      }
+    } catch (e) {
+      console.log(`Uploading file failed!`);
+      console.error(e);
+      console.log('Re-trying with endpoint: http://0.0.0.0:9000/upload...');
+      try {
+        data = await postRequest('http://0.0.0.0:9000/upload');
+      } catch (e) {
+        console.error(e);
+        console.log('Re-trying failed. Upload Failed!');
+      }
+    }
+    return data;
   }
 }
